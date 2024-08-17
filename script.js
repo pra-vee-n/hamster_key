ef73e47824dd5e5541504b6b4c8ccd2e9c4bdf02
PRAVEEN KUMAR <102923982+pra-vee-n@users.noreply.github.com>
2024-08-17 21:55:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -274,7 +274,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-        window.open('https://telegram.me/praveenkumar_26', '_blank');
+        window.open('https://telegram.me/Hamster_support_chat_bot', '_blank');
     });
 
     telegramChannelBtn.addEventListener('click', () => {
