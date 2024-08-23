f213e64346ff92283b8049b7b785cac6182a38ca
PRAVEEN KUMAR <102923982+pra-vee-n@users.noreply.github.com>
2024-08-23 16:00:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,15 +58,15 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Mow and Trim',
             appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            timing: 20000, // 20 seconds
-            attempts: 20,
+            eventsDelay: 20000, // 20 seconds
+            attemptsNumber: 20,
         },
         9: {
             name: 'Mud Racing',
             appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
             promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            timing: 20000, // 20 seconds
-            attempts: 20,
+            eventsDelay: 20000, // 20 seconds
+            attemptsNumber: 20,
         }
     };
 
