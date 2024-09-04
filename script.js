e83cb50686cdb48ddc117fb90ff07dce88a8fffc
PRAVEEN KUMAR <102923982+pra-vee-n@users.noreply.github.com>
2024-09-04 14:13:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,42 +9,42 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             eventsDelay: 21000,
-            attemptsNumber: 23,
+            attemptsNumber: 22,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
             eventsDelay: 20000,
-            attemptsNumber: 10
+            attemptsNumber: 15
         },
         3: {
-            name: 'Cafe Dash',
-            appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            eventsDelay: 20000, // 20 seconds
-            attemptsNumber: 20,
+            name: 'Fluff Crusade',
+            appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            eventsDelay: 23000,
+            attemptsNumber: 18,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
             eventsDelay: 20000,
-            attemptsNumber: 10,
+            attemptsNumber: 15,
         },
         5: {
             name: 'MergeAway',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
             eventsDelay: 20000,
-            attemptsNumber: 10,
+            attemptsNumber: 15,
         },
         6: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             eventsDelay: 20000,
-            attemptsNumber: 10,
+            attemptsNumber: 15,
             
         },
          7 : {
@@ -52,21 +52,21 @@ document.addEventListener('DOMContentLoaded', () => {
         appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         eventsDelay: 20000,
-        attemptsNumber: 16,
+        attemptsNumber: 18,
          },
         8: {
             name: 'Mow and Trim',
             appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             eventsDelay: 20000, // 20 seconds
-            attemptsNumber: 20,
+            attemptsNumber: 17,
         },
         9: {
-            name: 'Gangs Wars',
-            appToken: 'b6de60a0-e030-48bb-a551-548372493523',
-            promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
+            name: 'Tile Trio',
+            appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+            promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             eventsDelay: 40000,
-            attemptsNumber: 30,
+            attemptsNumber: 22,
         },
     };
 
