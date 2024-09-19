1cbb0cb08ea486e8b226ca2b719245c8968d447e
PRAVEEN KUMAR <102923982+pra-vee-n@users.noreply.github.com>
2024-09-19 13:00:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            eventsDelay: 20000,
+            eventsDelay: 30000,
             attemptsNumber: 15,
         },
         5: {
